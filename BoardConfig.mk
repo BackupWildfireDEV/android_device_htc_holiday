1d8bd7abd7b2ddfe3be685ea4fa6df8b29aa0aa2
WildfireDEV <wildfiredev@gmail.com>
2014-12-05 18:11:25 +1100

    Disable CUSTOM TOOLCHAIN temporarily

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@ BOARD_PAGE_SIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=holiday no_console_suspend=1
 TARGET_KERNEL_SOURCE := kernel/htc/holiday
 TARGET_KERNEL_CONFIG := holiday_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.9-cortex-a8
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.9-cortex-a8
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
